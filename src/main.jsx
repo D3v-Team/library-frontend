c7ffde664cba2c95169cac17c7cfa51bfa765b70
Doston <doston@Mac.mshome.net>
2026-08-18 17:53:36 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./i18n";
+import "./I18n";
 
 import App from "./App.jsx";
 import { ThemeProvider } from "@material-tailwind/react";
